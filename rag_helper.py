478ad9d2f0c2d139c1f29f2b3b2950253cc8b1a7
aryav <104564110+aryav-hash@users.noreply.github.com>
2026-07-16 22:11:45 +0530

    feat: Made the rag_helper.py file that has the RAGBase class with all the different function components that perform the operations needed for the RAG operation such as build_context, search, build_prompt, llm, rag. This file can now be used to do RAG from any notebook.

--- rag_helper.py
+++ rag_helper.py
@@ -1,4 +1,5 @@
 from ingest import load_faq_data, build_index
+from google.genai import types
 
 documents = load_faq_data()
 index = build_index(documents)
@@ -13,47 +14,78 @@ INSTRUCTIONS = """
 """
 
 USER_PROMPT_TEMPLATE = '''
-        Question:
-        {question}
+    Question:
+    {question}
 
-        Context:
-        {context}
-    '''
+    Context:
+    {context}
+'''
 
-def search(question, course="llm-zoomcamp"):
-    boost_dict = {"question": 2.0, "section": 0.5}
-    filter_dict = {"course": course}
+class RAGBase:
+    def __init__(self, index, llm_client, instructions=INSTRUCTIONS, 
+                 prompt_template=USER_PROMPT_TEMPLATE, course="llm-zoomcamp",model="gemini-2.5-flash"):
+        self.index = index
+        self.llm_client = llm_client
+        self.instructions = instructions
+        self.course = course
+        self.prompt_template = prompt_template
+        self.model = model
 
-    return index.search(
-        question,
-        boost_dict=boost_dict,
-        filter_dict=filter_dict, 
-        num_results=5
-    )
+    def search(self, question, course="llm-zoomcamp"):
+        boost_dict = {"question": 2.0, "section": 0.5}
+        filter_dict = {"course": self.course}
 
-def build_context(search_results):
-    lines = []
+        return self.index.search(
+            question,
+            boost_dict=boost_dict,
+            filter_dict=filter_dict, 
+            num_results=5
+        )
 
-    for doc in search_results:
-        lines.append(doc["section"])
-        lines.append("Q: " + doc["question"])
-        lines.append("A: " + doc["answer"])
-        lines.append("")
+    def build_context(self, search_results):
+        lines = []
 
-    return "\n".join(lines).strip()
+        for doc in search_results:
+            lines.append(doc["section"])
+            lines.append("Q: " + doc["question"])
+            lines.append("A: " + doc["answer"])
+            lines.append("")
 
-def build_prompt(question_local, search_result):
-    context_local = build_context(search_result)
-    USER_PROMPT_TEMPLATE = f"""
-        Question:
-        {question_local}
+        return "\n".join(lines).strip()
 
-        Context:
-        {context_local}
-    """
+    def build_prompt(self, question_local, search_result):
+        context_local = self.build_context(search_result)
+        USER_PROMPT_TEMPLATE = f"""
+            Question:
+            {question_local}
+
+            Context:
+            {context_local}
+        """
+        
+        prompt = USER_PROMPT_TEMPLATE.format(
+            question=question_local,
+            context_local=context_local
+        )
+        return prompt.strip()
     
-    prompt = USER_PROMPT_TEMPLATE.format(
-        question=question_local,
-        context_local=context_local
-    )
-    return prompt.strip()
+    def llm(self, instructions, user_prompt, model):
+        # message_history = [
+        #     {"role": "developer", "content": instructions},
+        #     {"role": "user", "content": user_prompt}
+        # ]
+        response = self.llm_client.models.generate_content(
+            model=model,
+            contents=user_prompt,
+            config=types.GenerateContentConfig(
+                system_instruction=instructions
+            )
+        )
+        
+        return response.text
+    
+    def rag(self, query, model="gemini-2.5-flash"):
+        search_results = self.search(query)
+        prompt_local = self.build_prompt(query, search_results)
+        answer = self.llm(INSTRUCTIONS, prompt_local, model)
+        return answer
